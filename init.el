5d4af079902e66b257415d99d883629e6f0e454a
saratbhargava <sarat@lecida.com>
2019-07-13 10:36:33 -0700

    Added more config to emacs

--- init.el
+++ init.el
@@ -1,6 +1,52 @@
 ;; init.el --- Emacs configuration
 
-;; package
+;; Melpa settings
+(require 'package)
+;(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; spacemacs theme
+(unless (package-installed-p 'spacemacs-theme)
+  (package-refresh-contents)
+  (package-install 'spacemacs-theme))
+
+;; solarized-theme
+(unless (package-installed-p 'solarized-theme)
+  (package-refresh-contents)
+  (package-install 'solarized-theme))
+
+;; load config from org mode file
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
+;; open emacs in full screen mode
+(add-to-list 'default-frame-alist '(fullscreen . fullboth))
+
+;; remove highlighting the text b/w point and mark
+(transient-mark-mode -1)
+
+;; inhibit splash screen 
+(setq inhibit-startup-screen t)
+(put 'narrow-to-region 'disabled nil)
+(put 'erase-buffer 'disabled nil)
+
+;; ispell config
+(setq ispell-program-name "/usr/local/bin/ispell")
+
+
+;; solarized theme
+;; (load-theme 'solarized-light t)
+;;; M-x disable-theme
+;;; enter the theme name you want to disable
+;; (load-theme 'solarized-dark t)
+;; (load-theme 'spacemacs-light t)
+(load-theme 'spacemacs-dark t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
